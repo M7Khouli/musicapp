6a8d4692bad37f9c237eac80ab6d4f01c0dec2ec
Mohammed Al Khouli <136886261+M7Khouli@users.noreply.github.com>
2023-09-25 15:16:11 +0300

    Delete getAllgenre.js

--- getAllgenre.js
+++
@@ -1,8 +0,0 @@
-const superAgent = require('superagent');
-const fs = require('fs');
-
-superAgent
-  .get('https://api.spotify.com/v1/recommendations/available-genre-seeds')
-  .then((err, res) => {
-    console.log(res.body);
-  });
